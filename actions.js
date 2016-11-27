dfab0b2082149d4509cef9eaeda97300f5f4d6c2
rdominguez <rdominguez@tecnologer.net>
2016-11-27 16:48:16 +0300

    making movements

--- actions.js
+++ actions.js
@@ -1,10 +1,11 @@
 (function() {
     "use strict"
-    
+
     var body = document.getElementsByTagName("body");
-    body.onKeyDown = setDirecction;
+
     //position of snake
     var snake = {
+        body: [],
         posX: 0,
         posY: 0,
         goingTo: "E",
@@ -21,7 +22,10 @@
     var ctx = undefined;
     var canvas = undefined;
     var _t_ = undefined;
-
+    var width = 450;
+    var height = 250;
+    var _tTurnSouth_ = undefined;
+    var _counterSouth_ = 0;
     /**
      * @author [rey]
      * @date        [11/27/2016]
@@ -32,39 +36,20 @@
         canvas = document.getElementById('snake');
 
         if (canvas && canvas.getContext) {
+            //set size canvas
+            canvas.width = width;
+            canvas.height = height;
+            canvas.style.width = canvas.width + "px";
+            canvas.style.height = canvas.height + "px";
+            //get context
             ctx = canvas.getContext('2d');
             //draw snake
-            ctx.fillRect(snake.posX, snake.posY, snake.size * snake.length, snake.size);
+            //ctx.fillRect(snake.posX, snake.posY, snake.size * snake.length, snake.size);
+            buildSnake();
             //draw first square
             drawSquare();
-
-            _t_ = setInterval(function() {
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                switch (snake.goingTo) {
-                    //North
-                    case "N":
-                        snake.posY -= snake.size;
-                        break;
-                        //South
-                    case "S":
-                        snake.posY += snake.size;
-                        break;
-                        //East
-                    case "E":
-                        snake.posX += snake.size;
-                        break;
-                        //West
-                    case "W":
-                        snake.posX -= snake.size;
-                        break;
-
-                }
-                if ((snake.posY + snake.size) >= canvas.height || (snake.posY > 0 && (snake.posY - snake.size) < canvas.height) || (snake.posX + (snake.size * snake.length)) >= canvas.width || (snake.posX > 0 && (snake.posX - (snake.size * snake.length))) < canvas.width)
-                    clearInterval(_t_);
-
-                ctx.fillRect(snake.posX, snake.posY, snake.size * snake.length, snake.size);
-                ctx.fillRect(square.cPosX, square.cPosY, snake.size, snake.size);
-            }, 1000 / snake.speed);
+            //move snake
+            moveSnake();
         }
     }
     /**
@@ -93,7 +78,7 @@
      * @return      {Boolean}                        [description]
      */
     function isNotValidPosition(nPosX, nPosY) {
-        var isNotValid = nPosX % snake.size != 0 || nPosY % snake.size != 0;
+        var isNotValid = nPosX % snake.size != 0 || nPosY % snake.size != 0 || nPosX == nPosY;
 
         if (nPosX % snake.size == 0 && nPosY % snake.size == 0 && nPosX !== nPosY) {
             switch (snake.goingTo) {
@@ -127,6 +112,7 @@
      */
     function setDirecction(e) {
         var keyCode = e.keyCode || e.which;
+        clearInterval(_t_);
 
         switch (keyCode) {
             case 38:
@@ -134,6 +120,7 @@
                 break;
             case 40:
                 snake.goingTo = "S";
+                turnSouth();
                 break;
             case 37:
                 snake.goingTo = "W";
@@ -144,5 +131,73 @@
         }
     }
 
+    function buildSnake() {
+        for (var i = 1; i <= snake.length; i++) {
+            snake.body.push({
+                x: snake.size * i,
+                y: 0
+            });
+        }
+
+        drawSnake();
+    }
+
+    function drawSnake() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < snake.body.length; i++) {
+            switch (snake.goingTo) {
+                //North
+                case "N":
+                    snake.body[i].y -= snake.size;
+                    break;
+                    //South
+                case "S":
+                    snake.body[i].y += snake.size;
+                    break;
+                    //East
+                case "E":
+                    snake.body[i].x += snake.size;
+                    break;
+                    //West
+                case "W":
+                    snake.body[i].x -= snake.size;
+                    break;
+
+            }
+
+            // snake.body[i].x += snake.posX;
+            // snake.body[i].y += snake.posY;
+
+            ctx.fillRect(snake.body[i].x, snake.body[i].y, snake.size, snake.size);
+        }
+    }
+
+    function moveSnake() {
+        _t_ = setInterval(function() {
+            drawSnake();
+            ctx.fillRect(square.cPosX, square.cPosY, snake.size, snake.size);
+        }, 1000 / snake.speed);
+    }
+
+    function turnSouth() {
+        _counterSouth_ = snake.body.length-1;
+        var firstMove = true;
+        _tTurnSouth_ = setInterval(function() {
+            snake.body[_counterSouth_].y += snake.size;
+            console.log(snake.body[_counterSouth_]);
+            for(var a=snake.body.length-2;a>=0;a--){
+                snake.body[a].x += snake.size;
+            }
+
+            drawSnake();
+            _counterSouth_--;
+            if (_counterSouth_ < 0) {
+                clearInterval(_tTurnSouth_);
+                moveSnake();
+            }
+        }, 1000 / snake.speed);
+    };
+
     constructor();
+    document.addEventListener("keydown", setDirecction);
 })();
